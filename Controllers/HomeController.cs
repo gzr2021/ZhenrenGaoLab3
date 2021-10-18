4800bfa04224c0ad014ce4ff72022ad8f2dde615
gzr <gao00104@algonquinlive.com>
2021-10-18 17:37:55 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person)
+        {
+            return View(person);
+        }
+
+        public IActionResult Count(Person person)
+        {
+            return View(person);
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        [Required]
+        public string FirstName
+        {
+            get; set;
+        }
+        
+        [Required]
+        public string LastName
+        {
+            get; set;
+        }
+
+        [Required]
+        public int Age
+        {
+            get; set;
+        }
+        
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string EmailAddress
+        {
+            get; set;
+        }
+        
+        [Required]
+        [DataType(DataType.Date)]
+     /*   [DisplayFormat(DataFormatString = "{mm/dd/yyyy}")]*/
+        public string DOB
+        {
+            get; set;
+        }
+        
+        [Required]
+        [StringLength(100)]
+        public string Password
+        {
+            get; set;
+        }
+        
+        [Required]
+        public string Description
+        {
+            get; set;
+        }
+
+        [Required]
+        public int bottle
+        {
+            get; set;
+        }
+    }
+}
